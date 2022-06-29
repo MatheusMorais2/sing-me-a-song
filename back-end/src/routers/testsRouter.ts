0f121a7a7b626afbeff9fa4e115f5a84a511f17f
iagod <iago.tostes@cubos.io>
2022-06-29 10:27:04 -0300

    Preparando revisão de código

--- back-end/src/routers/testsRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import testsController from "../controllers/testsController.js";
-
-const testsRouter = Router();
-
-testsRouter.post("/reset-database", testsController.resetDatabase);
-testsRouter.post("/seed-database", testsController.seedDatabase);
-
-export default testsRouter;
